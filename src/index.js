47eea3290fd55ef7b792ece4637d42a5e950534d
hugo@DESKTOP-J7IENLF <cristian260893@gmail.com>
2021-02-23 12:15:23 -0500

    Estructura y Diseño

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { CalendarioApp } from './CalendarioApp';
 import reportWebVitals from './reportWebVitals';
-
+import   './styles.css'
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalendarioApp/>
   </React.StrictMode>,
   document.getElementById('root')
 );
